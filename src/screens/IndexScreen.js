5c9668cff5b2a7fcc521e9b9aef5974dd16c7d11
igorlakic2 <igor.lakic2@gmail.com>
2021-10-18 16:15:52 +0200

    test

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, { useState, useRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Tekst</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
---
+++ src/screens/editor.js
@@ -0,0 +1,41 @@
+// import { RichEditor, RichToolbar } from "react-native-pell-rich-editor";
+// const [article, setArticle] = useState("");
+// const RichText = useRef();
+
+{
+  /* <RichEditor
+        disabled={false}
+        // containerStyle={styles.editor}
+        ref={RichText}
+        // style={styles.rich}
+        placeholder={"Start Writing Here"}
+        onChange={(text) => setArticle(text)}
+        // editorInitializedCallback={editorInitializedCallback}
+        // onHeightChange={handleHeightChange}
+      />
+      <RichToolbar
+        // style={[styles.richBar]}
+        editor={RichText}
+        disabled={false}
+        iconTint={"purple"}
+        selectedIconTint={"pink"}
+        disabledIconTint={"purple"}
+        // onPressAddImage={onPressAddImage}
+        iconSize={40}
+        // actions={[
+        //   "insertVideo",
+        //   ...defaultActions,
+        //   actions.setStrikethrough,
+        //   actions.heading1,
+        // ]}
+        // map icons for self made actions
+        // iconMap={{
+        //   [actions.heading1]: ({ tintColor }) => (
+        //     <Text style={[styles.tib, { color: tintColor }]}>H1</Text>
+        //   ),
+        //   [actions.setStrikethrough]: strikethrough,
+        //   ["insertVideo"]: video,
+        // }}
+        // insertVideo={insertVideo}
+      /> */
+}
